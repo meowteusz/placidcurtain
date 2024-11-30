1c98e0a5549e662a8e582a46f1c29acb278de0bb
Mateusz Ryczek <mateusz@u.northwestern.edu>
2024-11-30 00:07:32 -0600

    Build out sitemap

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import StudentLogin from "@views/StudentLogin.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "student-login",
+      component: StudentLogin,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/admin",
+      name: "admin-login",
+      component: () => import("@views/AdminLogin.vue"),
+    },
+    {
+      path: "/admin/students",
+      name: "students",
+      component: () => import("@views/StudentOverview.vue"),
+    },
+    {
+      // We don't want a null student so if they browse
+      // manually to /student and don't supply an ID we
+      // send them to /students instead.
+      path: "/student",
+      redirect: { name: "/admin/students" },
+    },
+    {
+      path: "/student/:id",
+      name: "student",
+      component: () => import("@views/StudentDetail.vue"),
+    },
+    {
+      path: "/me",
+      name: "self-view",
+      component: () => import("@views/SelfView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
