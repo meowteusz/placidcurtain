a38d8562b6d27459957ce2a89febb9b6109e2d26
Mateusz Ryczek <mateusz@u.northwestern.edu>
2024-11-30 00:08:08 -0600

    Move to routerview only

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
+// Vue Imports
+import { RouterView } from 'vue-router'
 
-const profiles = ref([])
-
-async function getProfiles() {
-  const { data } = await supabase.from('profiles').select()
-  profiles.value = data
-}
-
-onMounted(() => {
-  getProfiles()
-})
 </script>
 
 <template>
-  <ul>
-    <li v-for="profile in profiles" :key="profile.id">{{ profile }}</li>
-  </ul>
+  <RouterView />
 </template>
