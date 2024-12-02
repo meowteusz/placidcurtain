83eaace99f6bbac0cf8bf77ddb29dbf2592cc0a7
Mateusz Ryczek <mateusz@u.northwestern.edu>
2024-12-02 17:41:33 -0600

    Implement github auth

--- src/views/StudentLogin.vue
+++ src/views/StudentLogin.vue
@@ -1,29 +1,35 @@
 <script setup>
 // Vue Imports
 import { ref } from 'vue'
+import { useRouter } from 'vue-router';
 
 // Library Imports
 import { supabase } from '@/supabaseClient'
 
-const loading = ref(false)
-const email = ref('')
+// Component Imports
+import GitHubLogo from '@/assets/GitHubLogo.vue';
+
+const isSigningIn = ref(false);
+const router = useRouter();
 
 async function login() {
     try {
-        loading.value = true
-        const { error } = await supabase.auth.signInWithOtp({
-            email: email.value,
-        })
-        if (error) throw error
-        alert('Check your email for the login link!')
-    } catch (error) {
-        if (error instanceof Error) {
-            alert(error.message)
+        isSigningIn.value = true;
+
+        const { error } = await supabase.auth.signInWithOAuth({ provider: 'github' });
+
+        if (!error) {
+            router.push("/me");
         }
+
+    } catch (error) {
+        console.error(error);
+        throw error;
     } finally {
-        loading.value = false
+        isSigningIn.value = false;
     }
 }
+
 </script>
 
 <template>
@@ -33,12 +39,17 @@ async function login() {
                 <img src="../assets/mlds.svg" alt="MLDS Logo" width="400">
 
                 <div style="min-width: 25%;" class="d-flex flex-column mt-4">
-                    <div>
-                        <input class="form-control" type="email" v-model="email" id="email_input" placeholder="Email">
-                    </div>
 
-                    <div class="mt-2 d-flex mx-auto w-50 justify-content-center">
-                        <button @click="login" class="btn btn-success w-100">Send Magic Link</button>
+                    <div class="mt-2 d-flex mx-auto justify-content-center">
+
+                        <button @click="login"
+                            class="btn btn-outline-nu w-100 d-inline-flex justify-content-center flex-row flex-no-wrap"
+                            style="align-items: center;">
+                            <GitHubLogo />
+                            &nbsp;
+                            Sign in with GitHub
+
+                        </button>
                     </div>
                 </div>
 
@@ -46,3 +57,28 @@ async function login() {
         </div>
     </div>
 </template>
+
+<style>
+.btn-outline-nu {
+    color: #2b004f;
+    border-color: #2b004f;
+}
+
+.btn-outline-nu:hover {
+    color: #fff;
+    background-color: #E4E0EE;
+    border-color: #B6ACD1;
+}
+
+.btn-outline-nu:focus,
+.btn-outline-nu.focus {
+    box-shadow: 0 0 0 0.2rem rgba(43, 0, 79, 0.5);
+}
+
+.btn-outline-nu:active,
+.btn-outline-nu.active {
+    color: #fff;
+    background-color: #2b004f;
+    border-color: #2b004f;
+}
+</style>
