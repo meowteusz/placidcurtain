25f03fdb073f6af987d60db763a714023cbc7927
Mateusz Ryczek <mateusz@u.northwestern.edu>
2024-12-02 17:41:02 -0600

    Add auth update logic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup>
 // Vue Imports
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import { useRouter } from 'vue-router';
 
+import { supabase } from '@/supabaseClient'
+// import { useAuthStore } from '@stores/session';
+
+// const store = useAuthStore();
+const router = useRouter();
+
+async function printLogin() {
+  const { data: { user } } = await supabase.auth.getUser();
+  console.log(user);
+}
+
+supabase.auth.onAuthStateChange((event) => {
+  if (event === 'SIGNED_IN') {
+    printLogin();
+    router.push('/me');
+  }
+});
 </script>
 
 <template>
