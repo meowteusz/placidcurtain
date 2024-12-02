23c6cda08839fcbb7c1511288c754e9436b00ea3
Mateusz Ryczek <mateusz@u.northwestern.edu>
2024-12-02 16:36:08 -0600

    Add authenticated routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,11 @@
+// Vue Imports
 import { createRouter, createWebHistory } from "vue-router";
+
+// Component Imports
 import StudentLogin from "@views/StudentLogin.vue";
+
+// Library Imports
+import { supabase } from "@/supabaseClient";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,28 +21,49 @@ const router = createRouter({
       component: () => import("@views/AdminLogin.vue"),
     },
     {
-      path: "/admin/students",
+      path: "/students",
       name: "students",
       component: () => import("@views/StudentOverview.vue"),
+      meta: { requiresAuth: true },
     },
     {
       // We don't want a null student so if they browse
       // manually to /student and don't supply an ID we
-      // send them to /students instead.
+      // send them to /admin/students instead.
       path: "/student",
-      redirect: { name: "/admin/students" },
+      redirect: { name: "/students" },
     },
     {
       path: "/student/:id",
       name: "student",
       component: () => import("@views/StudentDetail.vue"),
+      meta: { requiresAuth: true },
     },
     {
       path: "/me",
       name: "self-view",
       component: () => import("@views/SelfView.vue"),
+      meta: { requiresAuth: true },
     },
   ],
+});
+
+router.beforeEach(async (to, from) => {
+  // Only enter conditional if the page needs auth
+  if (to.meta.requiresAuth) {
+    const { data: { user } } = await supabase.auth.getUser();
+
+    if (user.aud !== "authenticated") {
+      switch (from.name) {
+        case "student-login":
+          return { name: "student-login" };
+        case "admin-login":
+          return { name: "admin-login" };
+        default:
+          router.push({ name: "student-login" });
+      }
+    }
+  }
 });
 
 export default router;
